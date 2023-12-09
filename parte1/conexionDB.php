5d08c3a309bcfbbe67f7f66a1dea1289c8b079ec
NaslyRamos <naslyramos404@gmail.com>
2023-12-09 18:49:12 -0500

    Parte1

---
+++ parte1/conexionDB.php
@@ -0,0 +1,6 @@
+<?php 
+    $db_host="localhost"; //ubicacion de la base de datos
+    $db_nombre="usuario"; //nombre de la base de datos
+    $db_usuario="root"; //usuario de la base de datos
+    $db_contra=""; //contraseña de la base de datos
+?>
